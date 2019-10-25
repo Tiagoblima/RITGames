75d6de6ecf58e160f2bc541ac0db8d90b4ef9d93
Tiago de Lima <tiago.blima@ufrpe.br>
2019-10-25 09:21:08 -0300

    - Pequenos ajustes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, url_for
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
+def run_start(name=None):
+    return index()
+
+
+@app.route('/index.html')
 def index(name=None):
     return render_template('index.html', name=name)
+
+
+@app.route('/start.html')
+def start(name=None):
+    return render_template('start.html', name=name)
 
 
 if __name__ == '__main__':
